4c003f84861d11f30b4f93f82970aa90026fba8f
Frzz23 <frenzdrez@gmail.com>
2025-01-12 19:53:09 +0545

    Getting Started

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BidBuzz.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}
