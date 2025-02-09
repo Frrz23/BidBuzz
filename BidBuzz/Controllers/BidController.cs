b9616dc92d53ba9f8cf9fce7b7b2b2036418a68d
Frzz23 <frenzdrez@gmail.com>
2025-02-09 19:57:47 +0545

    Controller fixes

---
+++ BidBuzz/Controllers/BidController.cs
@@ -0,0 +1,83 @@
+﻿//using DataAccess.Repositary;
+//using Microsoft.AspNetCore.Mvc;
+//using Models.Models;
+
+//namespace BidBuzz.Controllers
+//{
+//    public class CategoryController : Controller
+//    {
+//        private readonly IUnitOfWork _unitOfWork;
+
+//        public CategoryController(IUnitOfWork unitOfWork)
+//        {
+//            _unitOfWork = unitOfWork;
+//        }
+
+//        public async Task<IActionResult> Index()
+//        {
+//            var categories = await _unitOfWork.Categories.GetAllAsync();
+//            return View(categories);
+//        }
+
+
+//        public async Task<IActionResult> Upsert(int? id) {
+            
+//            if (id == null || id == 0)
+//            {
+//                return View(new Category());
+//            }
+//            else
+//            {
+//                var category = await _unitOfWork.Categories.GetByIdAsync(id);
+//                if (category == null)
+//                {
+//                    return NotFound();
+//                }
+//                return View(category);
+//            }
+//        }
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public async Task<IActionResult> Upsert(Category category)
+//        {
+//            if (ModelState.IsValid) {
+//                if (category.Id == 0)
+//                {
+//                    await _unitOfWork.Categories.AddAsync(category);
+//                }
+//                else
+//                {
+//                    _unitOfWork.Categories.Update(category);
+
+//                }
+//                await _unitOfWork.CompleteAsync();
+//                return RedirectToAction(nameof(Index));
+            
+
+//        }
+//            return View(category);
+//        }
+
+ 
+//        public async Task<IActionResult> Delete(int id)
+//        {
+//            var category = await _unitOfWork.Categories.GetByIdAsync(id);
+//            if (category == null)
+//            {
+//                return NotFound();
+//            }
+//            return View(category);
+//        }
+
+//        [HttpPost, ActionName("Delete")]
+//        [ValidateAntiForgeryToken]
+//        public async Task<IActionResult> DeleteConfirmed(int id)
+//        {
+           
+//                _unitOfWork.Categories.Delete(id);
+//                await _unitOfWork.CompleteAsync();
+            
+//            return RedirectToAction(nameof(Index));
+//        }
+//    }
+//  }
